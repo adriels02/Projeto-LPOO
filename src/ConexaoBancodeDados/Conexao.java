7f8855eeee9d87c94a3eda7c284ff705282355e1
Kamila Rocha <126933553+kamilarochapimentel@users.noreply.github.com>
2023-07-11 19:49:35 -0300

    atualizando a classe de  conexao ao banco de dados

--- src/ConexaoBancodeDados/Conexao.java
+++ src/ConexaoBancodeDados/Conexao.java
@@ -1,21 +1,32 @@
 package ConexaoBancodeDados;
 
-//import java.sql.connetion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
-	public static void main (String[] args) {
+	public static void main (String[] args) throws SQLException {
+		Connection conexao = null;
 	
 		try {
 			Class.forName("com.msql.jdbc.Driver");
-		} catch (ClassNotFoundException ex) {
 			
+			//String acesso = "jdbc;mysql://localhost:3306/overlook?user=root&password=#123Mila123*";
+			conexao = DriverManager.getConnection("jdbc;mysql://localhost/overlook", "nome", "senha");
+			//ResultSet rs
+			
+		} catch (ClassNotFoundException e) {
 			System.out.println ("Banco de dados não encontrado");
-		}
+			
+		} catch (SQLException e) {
+		    System.out.println ("Erro ao acessar o banco" + e.getMessage());
+		  
+		} finally {
+			if (conexao != null) {
+				conexao.close();
+			} 
 			
 		
-	
-		
-		
-		
-	}
-
+		}		
+    }
 }
