24a43d795c8cb5544507395cb853e14bacc75af0
Kamila Rocha <126933553+kamilarochapimentel@users.noreply.github.com>
2023-07-12 12:03:00 -0300

    atualizando codigo e inserindo interface de acesso ao sistema

--- src/bdConexao/AcessoBancodeDados.java
+++ src/bdConexao/AcessoBancodeDados.java
@@ -1,5 +1,33 @@
 package bdConexao;
 
-public class AcessoBancodeDados {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class AcessoBancodeDados{
+	
+	public Connection conexaoBD() {
+		Connection conexao = null;
+		
+		
+		try {
+			Class.forName("com.mysq.jdbc.Driver");
+			String acessoBD = "jdbc:mysql://localhost:3306/overlook?user=root&password=#123Mila123*";
+			conexao = DriverManager.getConnection(acessoBD);
+			
+		} catch (ClassNotFoundException e){
+		    System.out.println("Banco de dados não localizado");
+		
+			
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Ocorreu um erro ao cessar o Banco de Dados " + e.getMessage());
+		}
+		
+		return conexao;
+		
+		
+	}
 
 }
